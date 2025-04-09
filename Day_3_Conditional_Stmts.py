8bc178ae5faba9792b0023e64a2c49ea57d63d3c
vikas-kaushik <vikaskaushik.ymca@gmail.com>
2025-04-09 00:02:48 -0400

    Day 2/3 Python programming

---
+++ Day_3_Conditional_Stmts.py
@@ -0,0 +1,22 @@
+# Simple if else statement
+height = 90
+if height >=120:
+    print("You can ride the rollercoaster !!")
+else:
+    print("Rollercoaster is denied for you !!")
+
+# = assignment
+# == equal to comparison
+
+
+# Modulo Operator
+# % gives the remainder
+
+# if elif else
+
+######################################################
+
+# Logical Operators
+AND
+OR
+NOT
